c6e5c1308a6ad5e56d96625479952656cd5a11b8
Didi <rmeirellesprof@gmail.com>
2022-09-21 17:23:09 -0300

    UPDATE

--- classes/Utils.js
+++ classes/Utils.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 class Utils {
     static dateFormat(date) {
-        return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
-=======
-class Utils  {
-    static dateFormat(date)  {
-        return date.getDate()
-        +"/"
-        +(date.getMonth()+1)
-        +"/"+date.getFullYear()
-        +" "
-        +date.getHours()
-        +":"
-        +date.getMinutes();
->>>>>>> 6069ae00d6951e50d4a351874e934d40e6075350
+        return date.getDate() +
+        "/" + (date.getMonth() + 1) +
+        "/" + date.getFullYear() + 
+        ' ' + date.getHours() + 
+        ':' + date.getMinutes();
     }
 }
--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 let userController = new UserController("form-user-create", "form-user-update", "table-users")
-=======
-let userController = new UserController("form-user-create", "table-users")
->>>>>>> 6069ae00d6951e50d4a351874e934d40e6075350
